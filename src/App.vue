6f3049ee0da49854e70f62de7835b6d48c55f4eb
Nirvazure <nirvazure@github.com>
2020-01-02 00:05:09 +0800

    delete slave

--- src/App.vue
+++ src/App.vue
@@ -1,87 +1,83 @@
 <template>
-<v-app>
+  <v-app>
     <v-navigation-drawer v-model="leftDrawer" app clipped>
-        <v-img @click="$router.push({path: `/`})" class="mx-3 my-3" :src="avatar"></v-img>
-        <v-list>
-            <v-list-item-group v-model="item" color="teal">
-                <v-list-item v-for="(item, i) in items" :key="i" @click="routerChange(item.text)">
-                    <v-list-item-icon>
-                        <v-icon v-text="item.icon"></v-icon>
-                    </v-list-item-icon>
-                    <v-list-item-content>
-                        <v-list-item-title class="font-weight-bold" v-text="item.text"></v-list-item-title>
-                    </v-list-item-content>
-                </v-list-item>
-            </v-list-item-group>
-        </v-list>
+      <v-img @click="$router.push({path: `/`})" class="mx-3 my-3" :src="avatar"></v-img>
+      <v-list>
+        <v-list-item-group v-model="item" color="teal">
+          <v-list-item v-for="(item, i) in items" :key="i" @click="routerChange(item.text)">
+            <v-list-item-icon>
+              <v-icon v-text="item.icon"></v-icon>
+            </v-list-item-icon>
+            <v-list-item-content>
+              <v-list-item-title class="font-weight-bold" v-text="item.text"></v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+        </v-list-item-group>
+      </v-list>
     </v-navigation-drawer>
     <v-navigation-drawer v-model="rightDrawer" app dark right>
-        <v-img :src="avatar"></v-img>
-
+      <v-img :src="avatar"></v-img>
     </v-navigation-drawer>
     <v-app-bar app color="teal" dark clipped-left>
-        <v-app-bar-nav-icon @click="leftDrawer = !leftDrawer" />
-        <v-toolbar-title>ArtMind
-        </v-toolbar-title>
-        <v-spacer></v-spacer>
-        <v-badge color="teal" overlap >
-            <template v-slot:badge>0</template>
-            <v-icon>mdi-bell</v-icon>
-        </v-badge>
-        <v-btn icon @click="rightDrawer = !rightDrawer">
-            <v-icon>mdi-apps</v-icon>
-        </v-btn>
+      <v-app-bar-nav-icon @click="leftDrawer = !leftDrawer" />
+      <v-toolbar-title>ArtMind</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-badge color="teal" overlap>
+        <template v-slot:badge>0</template>
+        <v-icon>mdi-bell</v-icon>
+      </v-badge>
+      <v-btn icon @click="rightDrawer = !rightDrawer">
+        <v-icon>mdi-apps</v-icon>
+      </v-btn>
     </v-app-bar>
     <v-content dark>
-        <router-view></router-view>
+      <router-view></router-view>
     </v-content>
     <!-- <v-footer app color="teal darken-1" dark>
         <v-btn text>站酷</v-btn>
         <v-spacer></v-spacer>
         <div>&copy; {{ new Date().getFullYear() }}</div>
-    </v-footer> -->
-</v-app>
+    </v-footer>-->
+  </v-app>
 </template>
 
 <script>
 export default {
-    data: () => ({
-        avatar: require("@/assets/avatar.jpg"),
-        leftDrawer: true,
-        rightDrawer: false,
-        item: 1,
-        items: [{
-                text: "core",
-                icon: "mdi-gamepad-square"
-            },
-            {
-                text: "slave",
-                icon: "mdi-account"
-            },
-            {
-                text: "",
-                icon: "mdi-soccer"
-            },
-            {
-                text: "core2",
-                icon: "mdi-music"
-            },
-            {
-                text: "test",
-                icon: "mdi-palette"
-            }, {
-                text: 'DreamMap',
-                icon: 'mdi-map'
-            }
-        ]
-    }),
-    methods: {
-        routerChange(routeName) {
-            //为啥是个这个符号
-            this.$router.push({
-                path: `/${routeName}`
-            });
-        }
+  data: () => ({
+    avatar: require("@/assets/avatar.jpg"),
+    leftDrawer: true,
+    rightDrawer: false,
+    item: 1,
+    items: [
+      {
+        text: "core",
+        icon: "mdi-gamepad-square"
+      },
+      {
+        text: "",
+        icon: "mdi-soccer"
+      },
+      {
+        text: "core2",
+        icon: "mdi-music"
+      },
+      {
+        text: "test",
+        icon: "mdi-palette"
+      },
+      {
+        text: "DreamMap",
+        icon: "mdi-map"
+      }
+    ]
+  }),
+  methods: {
+    routerChange(routeName) {
+      //为啥是个这个符号
+      this.$router.push({
+        path: `/${routeName}`
+      });
     }
+  }
 };
 </script>
--- src/router/index.js
+++ src/router/index.js
@@ -1,39 +1,35 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import Index from '../views/Index.vue'
+import Vue from "vue";
+import VueRouter from "vue-router";
+import Index from "../views/Index.vue";
 
-Vue.use(VueRouter)
+Vue.use(VueRouter);
 
 const routes = [
   {
-    path: '/',
-    name: 'index',
+    path: "/",
+    name: "index",
     component: Index
   },
   {
-    path: '/core',
-    name: 'core',
-    component: () => import('../views/Core.vue')
+    path: "/core",
+    name: "core",
+    component: () => import("../views/Core.vue")
   },
   {
-    path: '/core2',
-    name: 'core2',
-    component: () => import('../views/Core2.vue')
+    path: "/core2",
+    name: "core2",
+    component: () => import("../views/Core2.vue")
   },
   {
-    path:'/test',
-    component:()=>import('../views/Test2')
-  },
-  {
-    path:'/slave',
-    component:()=>import('../views/Slave')
+    path: "/test",
+    component: () => import("../views/Test2")
   }
-]
+];
 
 const router = new VueRouter({
-  mode: 'history',
+  mode: "history",
   base: process.env.BASE_URL,
   routes
-})
+});
 
-export default router
+export default router;
